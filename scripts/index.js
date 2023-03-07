7ad2375d9b88e9f762aef005f4d1d5883ed96b21
Денис Илюшин <to@denisilyushin.ru>
2023-03-07 23:59:43 +0300

    upd: план разработки

--- scripts/index.js
+++ scripts/index.js
@@ -7,6 +7,19 @@ let popupUserJobInput = popupFormElement.userJob;
 let userNameElement = document.querySelector('.profile__user-name');
 let userJobElement = document.querySelector('.profile__user-job');
 
+// попап
+// 
+// 
+
+/* Надо:
+*  1. задать в начале кода попапа переменную с попапми на странице и с активными кнопками
+*  2. ? создать на странице по списку нужные попапы из темплейта
+*  3. написать универсальные функции showPopip и hidePopup с аргументами
+*  4. отладить работу
+*/ 
+
+
+// универсальные функции для попапа
 function showPopup() {
   popupElement.classList.add('popup_opened');
   popupUserNameInput.value = userNameElement.textContent;
@@ -17,13 +30,45 @@ function hidePopup() {
   popupElement.classList.remove('popup_opened');
 }
 
+
+profileEditButtonElement.addEventListener('click', showPopup);
+popupCloseButtonElement.addEventListener('click', hidePopup);
+
+// 
+// сабммит формы попапа
 function handleFormSubmit(evt) {
   evt.preventDefault();
   userNameElement.textContent = popupUserNameInput.value;
   userJobElement.textContent = popupUserJobInput.value;
   hidePopup();
 }
-
-profileEditButtonElement.addEventListener('click', showPopup);
-popupCloseButtonElement.addEventListener('click', hidePopup);
 popupFormElement.addEventListener('submit', handleFormSubmit);
+
+
+
+
+// 
+// toggle лайков
+let likeButtonsElement = document.querySelectorAll(".mesto__like-button");
+function toggleMestoLikedStatus(event) {
+  event.target.classList.toggle("mesto__like-button_liked");
+}
+// заменить на стрелочную функцию?
+likeButtonsElement.forEach((likeButton) => {
+  likeButton.addEventListener("click", toggleMestoLikedStatus);
+});
+
+
+// 
+// закрытие попапа при мисклике
+function hidePopupByClickOnOverlay(event) {
+  if (event.target !== event.currentTarget) {
+    return;
+  }
+  hidePopup();
+}
+popupElement.addEventListener("click", hidePopupByClickOnOverlay);
+
+// раздели на модули: попап, генерация каточек, обработка профиля?
+// напиши автогенерацию мест
+// напиши удаление мест
