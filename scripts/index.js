f10b17231130f2d4de94c891e4ae3cbe6f6d2769
Денис Илюшин <to@denisilyushin.ru>
2023-03-08 16:29:29 +0300

    upd: добавить обработку форм и кнопок (тестирование)

--- scripts/index.js
+++ scripts/index.js
@@ -1,74 +1,85 @@
+
+// переменные блока "Работа с формой профиля"
 const profileEditButtonElement = document.querySelector('.profile__edit-button');
-let popupElement = document.querySelector('.popup');
-const popupCloseButtonElement = popupElement.querySelector('.popup__close-button');
-const popupFormElement = document.forms.popupEditProfileInfo;
-let popupUserNameInput = popupFormElement.userName;
-let popupUserJobInput = popupFormElement.userJob;
-let userNameElement = document.querySelector('.profile__user-name');
-let userJobElement = document.querySelector('.profile__user-job');
-
-// попап
-// 
-// 
-
-/* Надо:
-*  1. задать в начале кода попапа переменную с попапми на странице и с активными кнопками
-*  2. ? создать на странице по списку нужные попапы из темплейта
-*  3. написать универсальные функции showPopip и hidePopup с аргументами
-*  4. отладить работу
-*/ 
+const popupUserElement = document.querySelector('#editProfile');
+const popupUserCloseButtonElement = popupUserElement.querySelector('.popup__close-button');
+const popupUserFormElement = document.forms.popupEditProfileInfo;
+const popupUserNameInput = popupUserFormElement.userName;
+const popupUserJobInput = popupUserFormElement.userJob;
+const userNameElement = document.querySelector('.profile__user-name');
+const userJobElement = document.querySelector('.profile__user-job');
 
 
-// универсальные функции для попапа
-function showPopup() {
-  popupElement.classList.add('popup_opened');
+
+// Лайки
+function addEventListenerToLikeButtons () {
+  const likeButtonsElement = document.querySelectorAll(".mesto__like-button");
+  likeButtonsElement.forEach((likeButton) => {
+    likeButton.addEventListener("click", function(event) {
+      event.target.classList.toggle("mesto__like-button_liked");
+    });
+  });
+}
+addEventListenerToLikeButtons()
+
+// Кнопки удаления
+function addEventListenerToDeleteButtons () {
+  const deleteButtonsElement = document.querySelectorAll(".mesto__delete-button");
+  deleteButtonsElement.forEach((deleteButton) => {
+    deleteButton.addEventListener("click", function(event) {
+      // event.target.;
+      console.log(`Удалить карточку ${event.target.parentElement.querySelector('.mesto__description').innerText}`)
+    });
+  });
+}
+addEventListenerToDeleteButtons()
+
+// Работа с формой профиля
+function showUserPopup() {
+  popupUserElement.classList.add('popup_opened');
   popupUserNameInput.value = userNameElement.textContent;
   popupUserJobInput.value = userJobElement.textContent;
 }
 
-function hidePopup() {
-  popupElement.classList.remove('popup_opened');
+function hideUserPopup() {
+  popupUserElement.classList.remove('popup_opened');
 }
 
-
-profileEditButtonElement.addEventListener('click', showPopup);
-popupCloseButtonElement.addEventListener('click', hidePopup);
-
-// 
-// сабммит формы попапа
-function handleFormSubmit(evt) {
+function handleUserFormSubmit(evt) {
   evt.preventDefault();
   userNameElement.textContent = popupUserNameInput.value;
   userJobElement.textContent = popupUserJobInput.value;
-  hidePopup();
+  hideUserPopup();
 }
-popupFormElement.addEventListener('submit', handleFormSubmit);
 
+profileEditButtonElement.addEventListener('click', showUserPopup);
+popupUserCloseButtonElement.addEventListener('click', hideUserPopup);
+popupUserFormElement.addEventListener('submit', handleUserFormSubmit);
 
+// Работа с формой карточки
+// переменные блока "Работа с формой карточки"
+const profileAddButtonElement = document.querySelector('.profile__add-button');
+const popupMestoElement = document.querySelector('#addMesto');
+const popupMestoCloseButtonElement = popupMestoElement.querySelector('.popup__close-button');
+const popupMestoFormElement = document.forms.popupAddMesto;
+const popupMestoNameInput = popupMestoFormElement.mestoName;
+const popupMestoJobInput = popupMestoFormElement.mestoUrl;
 
+function showMestoPopup() {
+  popupMestoElement.classList.add('popup_opened');
+}
 
+function hideMestoPopup() {
+  popupMestoElement.classList.remove('popup_opened');
+}
+
+function handleMestoFormSubmit(evt) {
+  evt.preventDefault();
 // 
-// toggle лайков
-let likeButtonsElement = document.querySelectorAll(".mesto__like-button");
-function toggleMestoLikedStatus(event) {
-  event.target.classList.toggle("mesto__like-button_liked");
+  hideMestoPopup();
 }
-// заменить на стрелочную функцию?
-likeButtonsElement.forEach((likeButton) => {
-  likeButton.addEventListener("click", toggleMestoLikedStatus);
-});
 
+profileAddButtonElement.addEventListener('click', showMestoPopup);
+popupMestoCloseButtonElement.addEventListener('click', hideMestoPopup);
+popupMestoFormElement.addEventListener('submit', handleMestoFormSubmit);
 
-// 
-// закрытие попапа при мисклике
-function hidePopupByClickOnOverlay(event) {
-  if (event.target !== event.currentTarget) {
-    return;
-  }
-  hidePopup();
-}
-popupElement.addEventListener("click", hidePopupByClickOnOverlay);
-
-// раздели на модули: попап, генерация каточек, обработка профиля?
-// напиши автогенерацию мест
-// напиши удаление мест
