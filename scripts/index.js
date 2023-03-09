e759d20b73ff0b2892ddf99ee62afbda592bb8cb
Денис Илюшин <to@denisilyushin.ru>
2023-03-09 22:32:44 +0300

    fix: проверить проект по чеклисту

--- scripts/index.js
+++ scripts/index.js
@@ -23,7 +23,7 @@ const popupMestoImageSource = popupMestoImageElement.querySelector('.mesto__imag
 const popupMestoImageTitle = popupMestoImageElement.querySelector('.mesto__heading_popup');
 
 // Первоначальная начинка страница по темплейту
-const placesList = document.querySelector('.places__list')
+const placesList = document.querySelector('.places__list');
 const initialCards = [
   {
     name: 'Архыз',
@@ -66,9 +66,10 @@ function addMesto(mestoObj) {
   const mesto = document.querySelector('#mesto').content.cloneNode(true);
   const mestoImage = mesto.querySelector('.mesto__image');
   const mestoTitle = mesto.querySelector('.mesto__heading');
+
   mesto.querySelector('.mesto__heading').textContent = mestoObj.name;
   mesto.querySelector('.mesto__image').src = mestoObj.link;
-  mesto.querySelector('.mesto__image').alt = `Фотография ${mestoObj.name}`
+  mesto.querySelector('.mesto__image').alt = `Фотография ${mestoObj.name}`;
   // добавляет слушателя на кнопку лайка
   mesto.querySelector('.mesto__like-button').addEventListener(
     "click", (event) => {
@@ -80,7 +81,7 @@ function addMesto(mestoObj) {
       event.target.parentElement.remove();
     });
   // добавляет слушателя на фотографию места
-  mestoImage.addEventListener('click', () => {
+  mestoImage.addEventListener('click', function() {
     popupMestoImageSource.src = mestoImage.src;
     popupMestoImageTitle.textContent = mestoTitle.textContent;
     showPopup(popupMestoImageElement);
@@ -90,14 +91,14 @@ function addMesto(mestoObj) {
 
 // добавляет первые карточки при загрузке страницы
 function _initPage(initialMestoList) {
-  initialMestoList.forEach(element => {
+  initialMestoList.forEach((element) => {
     addMesto(element);
   });
 }
-_initPage(initialCards)
+_initPage(initialCards);
 
 // обработка формы редактирования профиля
-profileEditButtonElement.addEventListener('click', () => {
+profileEditButtonElement.addEventListener('click', function() {
   popupUserNameInput.value = userNameElement.textContent;
   popupUserJobInput.value = userJobElement.textContent;
   showPopup(popupUserElement)
@@ -105,7 +106,7 @@ profileEditButtonElement.addEventListener('click', () => {
 popupUserCloseButtonElement.addEventListener('click', () => {
   hidePopup(popupUserElement)
 });
-popupUserFormElement.addEventListener('submit', (event) => {
+popupUserFormElement.addEventListener('submit', function(event) {
   event.preventDefault();
   userNameElement.textContent = popupUserNameInput.value;
   userJobElement.textContent = popupUserJobInput.value;
@@ -119,19 +120,19 @@ profileAddButtonElement.addEventListener('click', () => {
 popupMestoCloseButtonElement.addEventListener('click', () => {
   hidePopup(popupMestoElement)
 });
-popupMestoFormElement.addEventListener('submit', (event) => {
+popupMestoFormElement.addEventListener('submit', function(event) {
   event.preventDefault();
   mesto = {
     name: popupMestoNameInput.value,
     link: popupMestoUrlInput.value,
   }
   // Если данные были переданы в форму - создаем карточку Места на странице
-  if (Object.values(mesto).every(value => value !== null && value !== '')) {
+  if (Object.values(mesto).every((value) => value !== null && value !== '')) {
     addMesto(mesto)
   }
   // Сброс значений в полях ввода в форме добавления Места
   const formInputs = event.target.querySelectorAll('.form__input')
-  formInputs.forEach(element => {
+  formInputs.forEach((element) => {
     element.value = ''
   });
   hidePopup(popupMestoElement);
