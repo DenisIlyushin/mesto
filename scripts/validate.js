ecfe01454726a511ea17e6c5d6e326687b5b9ea2
Денис Илюшин <to@denisilyushin.ru>
2023-04-17 23:07:04 +0300

    feat: codestyle

--- scripts/validate.js
+++ scripts/validate.js
@@ -1,5 +1,3 @@
-// Управление состоянием кнопки сабимита формы
-// Проверка валидности полей
 import FormValidator from "./FormValidator.js";
 
 // Общая функция включения валидации форм на страницах
@@ -7,11 +5,11 @@ export default function enableValidation(configObj) {
   const formsList = Array.from(document.querySelectorAll(configObj.formSelector));
 
   formsList.forEach((form) => {
+    const formValidator = new FormValidator(form, configObj)
+
     form.addEventListener('submit', (event) => {
       event.preventDefault();
     });
-    const formValidator = new FormValidator(form, configObj)
-
     formValidator.enableValidation();
   }); 
 };
