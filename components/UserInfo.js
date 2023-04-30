b28916b612243b3cfc25d035b0aa4162ea06fc09
Denis Ilyushin <to@denisilyushin.ru>
2023-04-30 23:21:34 +0300

    upd: вопросик

--- components/UserInfo.js
+++ components/UserInfo.js
@@ -6,7 +6,7 @@ export default class UserInfo {
     this.#userName = document.querySelector(userNameSelector);
     this.#userJob = document.querySelector(userJobSelector);
   };
-
+  // todo обновление значений textcontent => #data? https://app.pachca.com/chats/3916083?message=48620375
   getUserInfo() {
     return {
       name: this.#userName.textContent,
