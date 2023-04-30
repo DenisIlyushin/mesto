bbc9203e3e31fb0f31984a31d1d2f8f6d43a7a59
Denis Ilyushin <to@denisilyushin.ru>
2023-04-30 22:12:56 +0300

    feat: добавить класс PopupWithForm
    Создайте класс PopupWithForm, который наследует от Popup. Этот класс:
    
    Кроме селектора попапа принимает в конструктор колбэк сабмита формы.
    Содержит приватный метод _getInputValues, который собирает данные всех полей формы.
    Перезаписывает родительский метод setEventListeners. Метод setEventListeners класса PopupWithForm должен не только добавлять обработчик клика иконке закрытия, но и добавлять обработчик сабмита формы.
    Перезаписывает родительский метод close, так как при закрытии попапа форма должна ещё и сбрасываться.

---
+++ components/PopupWithForm.js
@@ -0,0 +1,34 @@
+import Popup from './Popup.js';
+
+export default class PopupWithForm extends Popup {
+  #submitHandler
+  #form
+  #inputs
+  #data = {} // todo не уверен в синтаксисе, по наитию
+
+  constructor({ formSubmitCallback }, popupSelector) {
+    super(popupSelector);
+    this.#submitHandler = formSubmitCallback;
+    this.#form = document.querySelector('.form');
+    this.#inputs = this.#form.querySelectorAll('.form__input');
+  };
+
+  #getInputValues() {
+    this.#inputs.forEach((input) => {
+      this.#data[input.name] = input.value;
+    });
+    return this.#data;
+  };
+
+  setEventListeners() {
+    super.setEventListeners();
+    this.#form.addEventListener('submit', () => {
+      this.#submitHandler(); // todo вероятно потребуется аргумент
+    });
+  };
+
+  close() {
+    super.close();
+    this.#form.reset();
+  };
+}
