23149f39d3f10d3c5aca075ee4d95cff00297297
Denis Ilyushin <to@denisilyushin.ru>
2023-05-19 23:21:49 +0300

    feat: попробовать написать юнит-тест Api.js

--- src/components/Api.js
+++ src/components/Api.js
@@ -18,6 +18,14 @@ class Api {
     return error
   }
 
+  getCards() {
+    return fetch(`${
+        this.#baseUrl}/cards`,
+      {headers: this.#headers}
+    )
+      .then(this.#handleResponse);
+  }
+
   getUserInfo() {
     return fetch(
       `${this.#baseUrl}/users/me`,
@@ -28,16 +36,22 @@ class Api {
   }
 }
 
-const api = new Api({
-  baseUrl: 'https://mesto.nomoreparties.co/v1/cohort-66',
-  headers: {
-    authorization: 'dc6a4a93-0c58-4e81-85df-4663aee25693',
-    'Content-Type': 'application/json',
-  },
-})
-
-api.getUserInfo()
-  .then((userInfo) => {
-    console.log(userInfo)
+function test() {
+  // попытка unit-теста
+  const api = new Api({
+    baseUrl: 'https://mesto.nomoreparties.co/v1/cohort-66',
+    headers: {
+      authorization: 'dc6a4a93-0c58-4e81-85df-4663aee25693',
+      'Content-Type': 'application/json',
+    },
   })
-  .catch(console.log)
+
+  Promise.all([
+    api.getUserInfo(),
+    api.getCards()
+  ])
+    .then( console.log)
+    .catch(console.log)
+}
+
+test()
